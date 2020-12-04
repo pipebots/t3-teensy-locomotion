b9a28b1813abc297a76dcc25101fa5c6587a15f5
nfry321 <nickfry321@gmail.com>
2020-12-04 21:37:19 +0000

    rm "right" & "left" in variable names

--- teensy-locomotion/config.h
+++ teensy-locomotion/config.h
@@ -16,37 +16,37 @@ const float max_speed = 1.0; // m/s
 const float wheel_base = 0.08; // m
 
 // Motor 1
-const driver_type left_driver = h_bridge;
-const char *l_driver_name = "Left Motor";
-const char *l_driver_id = "Left SN754410";
-const unsigned int left_pin_en = 5;
-const unsigned int left_pin_a = 7;
-const unsigned int left_pin_b = 8;
-const unsigned int left_deadzone = 5;
+const char *driver_1_name = "Left Motor";
+const char *driver_1_id = "Left SN754410";
+const driver_type driver_1 = h_bridge;
+const unsigned int d1_pin_en = 5;
+const unsigned int d1_pin_a = 7;
+const unsigned int d1_pin_b = 8;
+const unsigned int d1_deadzone = 5;
 
 // Motor 2
-const driver_type right_driver = h_bridge;
-const char *r_driver_name = "Right Motor";
-const char *r_driver_id = "Right SN754410";
-const unsigned int right_pin_en = 23;
-const unsigned int right_pin_a = 17;
-const unsigned int right_pin_b = 16;
-const unsigned int right_deadzone =5;
+const char *driver_2_name = "Right Motor";
+const char *driver_2_id = "Right SN754410";
+const driver_type driver_2 = h_bridge;
+const unsigned int d2_pin_en = 23;
+const unsigned int d2_pin_a = 17;
+const unsigned int d2_pin_b = 16;
+const unsigned int d2_deadzone =5;
 
 // Encoder 1
-const char *l_encoder_name = "Left Encoder";
-const unsigned int l_encoder_pin_a = 1;
-const unsigned int l_encoder_pin_b = 2;
-const unsigned int l_counts_per_rev = 2500;
-const char *l_encoder_id = "";
-const bool l_inverse = false;
+const char *encoder_1_name = "Left Encoder";
+const char *encoder_1_id = "";
+const unsigned int en1_pin_a = 1;
+const unsigned int en1_pin_b = 2;
+const unsigned int en1_counts_per_rev = 2500;
+const bool en1_inverse = false;
 
 // Encoder 2
-const char *r_encoder_name = "Right Encoder";
-const unsigned int r_encoder_pin_a = 3;
-const unsigned int r_encoder_pin_b = 4;
-const unsigned int r_counts_per_rev = 2500;
-const char *r_encoder_id = "";
-const bool r_inverse = true;
+const char *encoder_2_name = "Right Encoder";
+const char *encoder_2_id = "";
+const unsigned int en2_pin_a = 3;
+const unsigned int en2_pin_b = 4;
+const unsigned int en2_counts_per_rev = 2500;
+const bool en2_inverse = true;
 
 #endif
